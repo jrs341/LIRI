ddcdb772d11909caa5c22a3838c5b7b108eb413a
Jason Sinn <jrs341@me.com>
2016-10-09 12:35:25 -0500

    created variables to store the individual twitter keys

--- liri.js
+++ liri.js
@@ -1,6 +1,19 @@
 // code to grab data from keys.js
-var twitterKeys = require('./keys.js');
+var keys = require('./keys.js');
 
-console.log(twitterKeys);
+// console.log(keys);
+
+var myKeys = keys.twitterKeys;
+
+// console.log(myKeys);
 
 // variable to store the keys
+var consumerKey = myKeys.consumer_key;
+var consumerSecret = myKeys.consumer_secret;
+var accessTokenKey = myKeys.access_token_key;
+var accessTokenSecret = myKeys.access_token_secret;
+
+console.log(consumerKey);
+console.log(consumerSecret);
+console.log(accessTokenKey);
+console.log(accessTokenSecret);
