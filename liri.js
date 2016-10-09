15c058041d4632d131967c6001495c2fa2f521fc
Jason Sinn <jrs341@me.com>
2016-10-09 13:40:08 -0500

    added switch case for user input

--- liri.js
+++ liri.js
@@ -3,17 +3,37 @@ var keys = require('./keys.js');
 
 // console.log(keys);
 
+// variable to store the twitter keys
 var myKeys = keys.twitterKeys;
 
 // console.log(myKeys);
 
-// variable to store the keys
+// variables to store the individual twitter keys
 var consumerKey = myKeys.consumer_key;
 var consumerSecret = myKeys.consumer_secret;
 var accessTokenKey = myKeys.access_token_key;
 var accessTokenSecret = myKeys.access_token_secret;
 
-console.log(consumerKey);
-console.log(consumerSecret);
-console.log(accessTokenKey);
-console.log(accessTokenSecret);
+// console.log(consumerKey);
+// console.log(consumerSecret);
+// console.log(accessTokenKey);
+// console.log(accessTokenSecret);
+
+var userCommand = process.argv[2];
+
+console.log(userCommand);
+
+switch (userCommand){
+	case 'my-tweets': console.log('case1 ' + userCommand);
+	break;
+
+	case 'spotify-this-song': console.log('case2 ' + userCommand);
+	break;
+
+	case 'movie-this': console.log('case3 ' + userCommand);
+	break;
+
+	case 'do-what-it-says': console.log('case4 ' + userCommand);
+	break;
+}
+// if (process.argv[2] == 'my-tweets');
